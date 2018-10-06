2d4092e184d0c7f5826d2570d4e992363bdd7ba6
Khadeeja Alif <khadeeja@fantacode.com>
2018-10-06 11:40:53 +0530

    Awo

---
+++ Bla/Haha.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Bla
+{
+    public partial class Haha : ContentPage
+    {
+        public Haha()
+        {
+            InitializeComponent();
+        }
+    }
+}
