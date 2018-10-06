efa9ab3ace690af29ab092df76967f5b95ec6381
Khadeeja Alif <khadeeja@fantacode.com>
2018-10-06 11:49:49 +0530

    fix

---
+++ Bla/Ugh.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Bla
+{
+    public partial class Ugh : ContentPage
+    {
+        public Ugh()
+        {
+            InitializeComponent();
+        }
+    }
+}
